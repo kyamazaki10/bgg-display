7052dc7020a8966864ed1c0e718b2e7a335936a6
Kumiko Yamazaki <kyamazaki@gmail.com>
2017-09-23 12:41:19 -0600

    master detail UI pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,28 +5,58 @@ export class User {
   name: string;
 }
 
+const USERS: User[] = [
+  { id: 11, name: 'kmeeks11' },
+  { id: 12, name: 'kmeeks12' },
+  { id: 13, name: 'kmeeks13' },
+  { id: 14, name: 'kmeeks14' },
+  { id: 15, name: 'kmeeks15' }
+];
+
 @Component({
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{user.name}} details</h2>
-    <div>
-      <label>id: </label>
-      {{user.id}}
-    </div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="user.name" placeholder="name">
+    <h2>List of Users</h2>
+    <ul class="users">
+      <li *ngFor="let user of users"
+        (click)="onSelect(user)"
+        [class.selected]="user === selectedUser">
+        <span class="id">{{user.id}}</span>
+        {{user.name}}</li>
+    </ul>
+
+    <div *ngIf="selectedUser">
+      <h2>{{selectedUser.name}} details</h2>
+      <div>
+        <label>id: </label>
+        {{selectedUser.id}}
+      </div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedUser.name" placeholder="name">
+      </div>
     </div>
   `,
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+    h1, h2 {
+      font-weight: normal;
+    }
+
+    li.selected {
+      color: #ff0000;
+    }
+  `]
 })
 
 export class AppComponent {
   title = 'BGG Display';
-  user: User = {
-    id: 1,
-    name: 'kmeeks'
-  };
+  selectedUser: User;
+  users = USERS;
+
+  onSelect(user: User): void {
+    this.selectedUser = user;
+  }
 }
