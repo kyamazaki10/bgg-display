7c2a12cb31a89379dcdc41902061cfd4887c4e93
Kumiko Yamazaki <kyamazaki@gmail.com>
2017-09-23 12:00:57 -0600

    basic editor and forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+export class User {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{user.name}} details</h2>
+    <div>
+      <label>id: </label>
+      {{user.id}}
+    </div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="user.name" placeholder="name">
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'BGG Display';
+  user: User = {
+    id: 1,
+    name: 'kmeeks'
+  };
 }
