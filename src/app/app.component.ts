7d43f4a22a34d883c8fe9b63e45535b5215aa10f
Kumiko Yamazaki <kyamazaki@gmail.com>
2017-09-27 21:05:34 -0600

    removing default template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/users" routerLinkActive="active">Users</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 
