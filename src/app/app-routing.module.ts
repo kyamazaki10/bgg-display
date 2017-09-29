b7e23e5542a399ac583e2ff98033e1bf6b49ac4d
Kumiko Yamazaki <kyamazaki@gmail.com>
2017-09-28 20:04:10 -0600

    main page and nav cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,6 @@ import { UsersComponent } from './users.component';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-  },
-  {
     path: 'dashboard',
     component: DashboardComponent
   },
