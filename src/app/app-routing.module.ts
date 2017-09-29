16858a4659e20df2d5ac96a13858b50d3ccb4ab6
Kumiko Yamazaki <kyamazaki@gmail.com>
2017-09-28 21:52:17 -0600

    adding ng-bootstrap!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { SearchComponent } from './search.component';
 import { UserDetailComponent } from './user-detail.component';
 import { UsersComponent } from './users.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: SearchComponent
+  },
   {
     path: 'dashboard',
     component: DashboardComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 // imports for loading and configuring in-memory web api
 import { InMemoryDataService } from './in-memory-data.service';
@@ -9,6 +10,7 @@ import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
+import { SearchComponent } from './search.component';
 import { UserDetailComponent } from './user-detail.component';
 import { UserSearchComponent } from './user-search.component';
 import { UserService } from './user.service';
@@ -20,6 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [
     AppComponent,
     DashboardComponent,
+    SearchComponent,
     UserDetailComponent,
     UserSearchComponent,
     UsersComponent
@@ -29,6 +32,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpModule,
+    NgbModule.forRoot(),
     InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [
