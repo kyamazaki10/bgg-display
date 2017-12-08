dec88e6839cbff26a5372b82aa944c41867d305f
Kumiko Yamazaki <kyamazaki@gmail.com>
2017-12-07 20:59:26 -0700

    fixing broken test

--- src/app/users/user-dashboard/user-dashboard.component.spec.ts
+++ src/app/users/user-dashboard/user-dashboard.component.spec.ts
@@ -1,9 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
+import { BggService } from './../../shared/services/bgg.service';
 import { UserDashboardComponent } from './user-dashboard.component';
 import { UserNavigationComponent } from './../user-navigation/user-navigation.component';
 import { UserService } from './../../shared/services/user.service';
@@ -21,10 +23,12 @@ describe('UserDashboardComponent', () => {
       imports: [
         BrowserAnimationsModule,
         FormsModule,
+        HttpModule,
         NgxChartsModule,
         RouterTestingModule
       ],
       providers: [
+        BggService,
         UserService
       ]
     })
